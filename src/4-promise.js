0f837c00cee8e30e21683ac2f5ca6fffbac33e19
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2019-06-10 09:43:05 +0200

    Added 4-promise

---
+++ src/4-promise.js
@@ -0,0 +1,42 @@
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const request = require('request');
+
+const { SERVER, names } = require('./config');
+
+
+const retrieve = name => new Promise(function(resolve, reject) {
+	request.get(`${SERVER}/${name}-sitemap.xml`, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			resolve(body);
+		} else {
+			reject(error);
+		}
+	});
+});
+
+const parseXmlAndGetUrls = xml => {
+	const doc = new dom().parseFromString(xml);
+
+	var nodes = xpath.select('//*[local-name(.)="url"]/*[local-name(.)="loc"]', doc);
+
+	const urls = nodes.map(node => node.textContent);
+
+	return urls;
+};
+
+const display = urls => {
+	urls.forEach(url => {
+		console.log(url);
+	});
+};
+
+const process = body => {
+	const urls = parseXmlAndGetUrls(body);
+	display(urls);
+};
+
+names.forEach(name => {
+	retrieve(name)
+		.then(xml => process(xml));
+});
